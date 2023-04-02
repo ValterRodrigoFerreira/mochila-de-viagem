7f3e23c5105f2d416e22ab8df0608f8556b05191
Valter <valter.rodrigo.ferreira@gmail.com>
2023-04-02 17:22:13 -0300

    capturando os dados na tela nome do item mais a quantidade do item

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Funcionou");
+const formulario = document.getElementById("novoItem");
+
+formulario.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+
+  console.log(evento);
+  console.log(evento.target.elements["nome"].value);
+  console.log(evento.target.elements["quantidade"].value);
+});
