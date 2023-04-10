fa243db531518b0d6e08d6e96383c8df6796054b
Valter <valter.rodrigo.ferreira@gmail.com>
2023-04-09 21:10:42 -0300

    projeto refeito até a parte de add um nove item para a lista de viagem, intuido disso é recuper algo feito anterior ao novo commit, simulando que tenhamos feito ao de errado.

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 const formulario = document.getElementById("formulario");
 const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
-
-itens.forEach((element) => {
-  console.log(element.nome, element.quantidade);
-});
 
 formulario.addEventListener("submit", (evento) => {
   evento.preventDefault();
@@ -13,29 +8,17 @@ formulario.addEventListener("submit", (evento) => {
   const quantidadeItem = evento.target.elements["quantidade"];
 
   criaItem(nomeItem.value, quantidadeItem.value);
-
-  nomeItem.value = "";
-  quantidadeItem.value = "";
 });
 
 function criaItem(nomeItem, quantidadeItem) {
-  const novoItem = document.createElement("li");
+  novoItem = document.createElement("li");
   novoItem.classList.add("item");
 
-  const numeroItem = document.createElement("strong");
+  numeroItem = document.createElement("strong");
+
   numeroItem.innerHTML = quantidadeItem;
 
   novoItem.appendChild(numeroItem);
-
   novoItem.innerHTML += nomeItem;
   lista.appendChild(novoItem);
-
-  const itemAtual = {
-    nomeItem: nomeItem,
-    quantidadeItem: quantidadeItem,
-  };
-
-  itens.push(itemAtual);
-
-  localStorage.setItem("itens", JSON.stringify(itens));
 }
